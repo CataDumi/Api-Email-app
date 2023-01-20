46ea832b84d7d09182f6a4609432406df89d5907
CataDumi <cata.dumi.134@gmail.com>
2023-01-20 11:29:17 +0200

    3rd commit rewrite some lines

--- main.py
+++ main.py
@@ -1,27 +1,17 @@
 import requests
 from send_email import send_email
 
-url='https://newsapi.org/v2/everything?' \
-    'q=tesla&from=2022-12-19&' \
-    'sortBy=publishedAt&' \
-    'apiKey=94f415fe5e694f04a9fb7ae9e036810e&' \
-    'language=en'
-apiKEY='94f415fe5e694f04a9fb7ae9e036810e'
+url = 'https://newsapi.org/v2/everything?' \
+      'q=tesla&from=2022-12-19&' \
+      'sortBy=publishedAt&' \
+      'apiKey=94f415fe5e694f04a9fb7ae9e036810e&' \
+      'language=en'
+apiKEY = '94f415fe5e694f04a9fb7ae9e036810e'
 
-header={
-    'apiKey':'94f415fe5e694f04a9fb7ae9e036810e',
-    'q':'tesla',
-    'from':'2023-01-18',
-    'language':'en',
-    'sortBy':'popularity'
-
-}
-
-
-response= requests.get(url=url)
+response = requests.get(url=url)
 print(response.raise_for_status())
-content=response.json()
-content=content['articles']
+content = response.json()
+content = content['articles']
 
 def format_article(article):
     formatted_article = f'''
@@ -34,12 +24,10 @@ def format_article(article):
     '''
     return formatted_article
 
-txt=''
-for article in content[:20]: # asa limitezi sa primesti doar primele 20 de iteme din lista
-    art=format_article(article)
-    txt+=art
+txt = ''
+for article in content[:20]:  # asa limitezi sa primesti doar primele 20 de iteme din lista
+    art = format_article(article)
+    txt += art
 
 txt = txt.encode('utf-8')
 send_email(message=txt)
-
-
